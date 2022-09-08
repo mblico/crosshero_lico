47ce1baad0f8889e63b7f529dd9150b61bc867cf
mbelenlico <mblico@icloud.com>
2022-09-08 17:00:51 -0300

    subo archivos y seo

---
+++ script.js
@@ -0,0 +1,9 @@
+let menu = document.querySelector('#menu');
+
+
+let menuBar = document.querySelector('#menuBar');
+
+menuBar.addEventListener('click', function() {
+    menu.classList.toggle("menu-toggle");
+    
+  });
